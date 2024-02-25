5162e22c64df2c16578a7708cf3ce51a93738f10
Serhii Seletskyi <SeletskySergey@live.com>
2024-02-25 20:21:48 +0200

    fixed

--- components/airmaster/__init__.py
+++ components/airmaster/__init__.py
@@ -20,7 +20,6 @@ LED_OUTPUT_SCHEMA = vol.Schema({
 
 CONFIG_SCHEMA = sensor.sensor_schema(AirMasterSensor).extend({
     vol.Optional("led_output"): LED_OUTPUT_SCHEMA,
-    # Additional sensor configuration schema extensions
 }).extend(uart.UART_DEVICE_SCHEMA)
 
 def to_code(config):
@@ -31,52 +30,3 @@ def to_code(config):
     if "led_output" in config:
         led = yield cg.get_variable(config["led_output"])
         cg.add(var.set_led_output(led))
-    # Additional code generation logic for sensors
-
-
-
-
-
-
-
-
-# CONFIG_SCHEMA = sensor.sensor_schema(AirMasterSensor)
-
-# # Dynamically extend the schema with sensor configurations
-# for sensor_name, (unit, icon, decimals, device_class) in SENSOR_TYPES.items():
-#     CONFIG_SCHEMA.extend({
-#         vol.Optional(sensor_name): sensor.sensor_schema(
-#             unit_of_measurement=unit,
-#             icon=icon,
-#             accuracy_decimals=decimals,
-#             device_class=device_class
-#         )
-#     })
-
-# LED_OUTPUT_SCHEMA = vol.Schema({
-#     vol.Required(CONF_ID): core.ID,
-# })
-
-# # Extend the schema with LED output configuration
-# CONFIG_SCHEMA.extend({
-#     vol.Optional("led_output"): LED_OUTPUT_SCHEMA,
-# })
-
-# # Extend the schema with UART device configuration
-# CONFIG_SCHEMA.extend(uart.UART_DEVICE_SCHEMA)
-
-# def to_code(config):
-#     var = cg.new_Pvariable(config[CONF_ID])
-#     yield cg.register_component(var, config)
-#     yield uart.register_uart_device(var, config)
-
-#     for sensor_name, (unit, icon, decimals, device_class) in SENSOR_TYPES.items():
-#         if sensor_name in config:
-#             conf = config[sensor_name]
-#             sens = yield sensor.new_sensor(conf)
-#             cg.add(getattr(var, f'set_{sensor_name}')(sens))
-    
-#     if "led_output" in config:
-#         led_conf = config["led_output"]
-#         led = yield cg.get_variable(led_conf[CONF_ID])
-#         cg.add(var.set_led_output(led))
