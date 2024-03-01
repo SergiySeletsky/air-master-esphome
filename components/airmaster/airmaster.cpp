1baaa312936245439ac4dc63a057c28ece86a773
Serhii Seletskyi <SeletskySergey@live.com>
2024-03-01 22:07:00 +0200

    fixed check

--- components/airmaster/airmaster.cpp
+++ components/airmaster/airmaster.cpp
@@ -31,9 +31,8 @@ namespace esphome
           calculated_checksum += response[i];
         }
 
-        if (received_checksum == calculated_checksum && received_checksum != 243)
+        if (received_checksum == calculated_checksum && received_checksum != 243 && received_checksum != 680) // 243 and 680 are common checksums when the sensor is not connected
         {
-          ESP_LOGW(TAG, "Check %d", received_checksum);
           // Process and publish sensor data
           pm25_sensor->publish_state(response[2] | response[1] << 8);
           pm10_sensor->publish_state(response[4] | response[3] << 8);
